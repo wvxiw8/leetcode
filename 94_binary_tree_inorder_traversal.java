d80abb7f408351416d2842ba8f969613a1b66d85
wvxiw <wvxiw8@gmail.com>
2023-11-17 01:42:12 +0300

    Added non-recursive traversal for 94_binary_tree_inorder_traversal.java

--- 94_binary_tree_inorder_traversal.java
+++ 94_binary_tree_inorder_traversal.java
@@ -43,6 +43,7 @@ import com.wvxiw.leetcode.datastructures.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Definition for a binary tree node.
@@ -70,11 +71,36 @@ class Solution {
         traverseLnr(n.right, output);
     }
 
+    // Using recursion
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         traverseLnr(root, list);
         return list;
     }
+
+    // Without recursion
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null)
+            return list;
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        stack.push(curr);
+        curr = curr.left;
+
+        while (!stack.isEmpty() || curr != null) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            TreeNode n = stack.pop();
+            list.add(n.val);
+            curr = n.right;
+        }
+        return list;
+    }
+
 
     public static void main(String[] args) {
         TreeNode n3 = new TreeNode(3, null, null);
@@ -82,7 +108,8 @@ class Solution {
         TreeNode n1 = new TreeNode(1, null, n2);
 
         List<Integer> exp = List.of(1, 3, 2);
-        List<Integer> result = new Solution().inorderTraversal(n1);
+//        List<Integer> result = new Solution().inorderTraversal(n1);
+        List<Integer> result = new Solution().inorderTraversal2(n1);
         if (result.equals(exp))
             System.out.println("OK");
         else
