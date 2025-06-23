733aaa17df039a240217855f3b63e3a650658531
wvxiw <wvxiw8@gmail.com>
2025-06-24 02:16:41 +0300

    added methods to init and print cycled list

--- lib/go/list/list.go
+++ lib/go/list/list.go
@@ -1,5 +1,7 @@
 package list
 
+import "fmt"
+
 // Definition for singly-linked list.
 type ListNode struct {
 	Val  int
@@ -38,3 +40,59 @@ func ListToSlice(head *ListNode) []int {
 	return a
 }
 
+// NewListCycled creates a linked list with the tail element pointing to the [cycle]. Uncycled if cycle=-1
+func NewListCycled(data []int, cycle int) *ListNode {
+	var node *ListNode = nil
+	var next *ListNode = nil
+	var cycling *ListNode = nil
+	var last *ListNode = nil
+	n := len(data) - 1
+	for i := n; i >= 0; i-- {
+		node = &ListNode{data[i], next}
+		if i == cycle {
+			cycling = node
+		} else if i == n {
+			last = node
+		}
+		next = node
+	}
+	if cycle != -1 {
+		last.Next = cycling
+	}
+	return node
+}
+
+func (l *ListNode) Print(max int) {
+
+	if l == nil {
+		fmt.Println("[nil node]")
+		return
+	}
+	for i := 0; i < max; i++ {
+		fmt.Printf("[%d] %p %d\n", i, l, l.Val)
+		if l.Next == nil || i > max {
+			break
+		}
+		l = l.Next
+	}
+}
+func (l *ListNode) PrintNoCycle(max int) {
+	m := make(map[*ListNode]*ListNode)
+
+	if l == nil {
+		fmt.Println("[nil node]")
+		return
+	}
+	for i := 0; i < max; i++ {
+		if _, exist := m[l]; exist {
+			fmt.Printf("[%d] cycle detected; prev node points to %p (value = %v)\n", i, l, l.Val)
+			return
+		}
+		m[l] = l
+		fmt.Printf("[%d] %p %d\n", i, l, l.Val)
+		if l.Next == nil || i > max {
+			break
+		}
+		l = l.Next
+	}
+}
